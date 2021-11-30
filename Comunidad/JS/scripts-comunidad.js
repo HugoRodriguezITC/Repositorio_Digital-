0b020ef32c833ff541a78cf07e89bfc516a844fd
HugoRodriguezITC <17680215@cuautla.tecnm.mx>
2021-11-30 17:35:42 -0600

    Commit 13 From VS Firts Version

---
+++ Comunidad/JS/scripts-comunidad.js
@@ -0,0 +1,9 @@
+function HoverFlechas() {
+    document.getElementById('hover-left').style.visibility = 'visible';
+    document.getElementById('hover-right').style.visibility = 'visible';
+}
+
+function MoveMouse() {
+    document.getElementById('hover-left').style.visibility = 'hidden';
+    document.getElementById('hover-right').style.visibility = 'hidden';
+}
---
+++ scripts-home.js
@@ -0,0 +1,19 @@
+function HoverFlechas() {
+    document.getElementById('hover-left').style.visibility = 'visible';
+    document.getElementById('hover-right').style.visibility = 'visible';
+}
+
+function MoveMouse() {
+    document.getElementById('hover-left').style.visibility = 'hidden';
+    document.getElementById('hover-right').style.visibility = 'hidden';
+}
+
+function HoverFlechas1() {
+    document.getElementById('hover-left-1').style.visibility = 'visible';
+    document.getElementById('hover-right-1').style.visibility = 'visible';
+}
+
+function MoveMouse1() {
+    document.getElementById('hover-left-1').style.visibility = 'hidden';
+    document.getElementById('hover-right-1').style.visibility = 'hidden';
+}
